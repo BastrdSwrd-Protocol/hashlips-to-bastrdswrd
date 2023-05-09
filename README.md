9234331cce981944e38ad3fafa4548b380e25d33
TrustMountains <132965097+TrustMountains@users.noreply.github.com>
2023-05-08 16:19:42 -1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# HashLips Art Engine to thirdweb
+
+CSV formatter, from JSON to CSV, making the CSV ready for thirdweb bulk upload.
+
+It takes a json file (modify metadata.json) and outputs a CSV file.
+
+Once you have updated your .json file, run the following commands:
+
+### npm install
+
+### npx ts-node format.ts
---
+++ format.ts
@@ -0,0 +1,38 @@
+import data from "./metadata.json";
+import { createObjectCsvWriter } from "csv-writer";
+
+const attributes = data[0].attributes.map((attribute: { trait_type: string, value: string; }) => attribute.trait_type);
+
+const formatJson = (json: any) => {
+  return json.map((nft: any) => {
+
+    const formattedNft = {
+      name: nft.name,
+      description: nft.description,
+      dna: nft.dna,
+      edition: nft.edition,
+      date: nft.date,
+    };
+
+    attributes.map((attribute: any) => {
+      const findAttribute = nft.attributes.find(({ trait_type }: { trait_type: string; }) => trait_type === attribute);
+      const value = findAttribute?.value || "";
+      formattedNft[attribute] = value.trim() === "None" ? "" : value.trim();
+    });
+    return formattedNft;
+  });
+};
+
+const metadata = formatJson(data);
+
+const header = [
+  { id: 'name', title: 'name' },
+  { id: 'description', title: 'description' },
+  { id: 'dna', title: 'dna' },
+  { id: 'edition', title: 'edition' },
+  { id: 'date', title: 'date' },
+];
+
+attributes.map((attribute: string) => header.push({ id: attribute, title: attribute }));
+
+createObjectCsvWriter({ path: "./metadata.csv", header }).writeRecords(metadata).then(() => console.log('The CSV file was written successfully'));
